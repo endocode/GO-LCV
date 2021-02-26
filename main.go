efd67ff3c88a12e0be0d0fac0583c5d908832f67
michele scarlato <michele.scarlato@gmail.com>
2021-02-26 19:31:24 +0900

    CSV into struct approach

--- main.go
+++ main.go
@@ -3,27 +3,17 @@ package main
 import (
 	"encoding/csv"
 	"encoding/json"
+
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 )
 
-func readCsvFile(filePath string) [][]string {
-	f, err := os.Open(filePath)
-	if err != nil {
-		log.Fatal("Unable to read input file "+filePath, err)
-	}
-	defer f.Close()
-
-	csvReader := csv.NewReader(f)
-	records, err := csvReader.ReadAll()
-	if err != nil {
-		log.Fatal("Unable to parse file as CSV for "+filePath, err)
-	}
-
-	return records
+type CsvLine struct {
+	LicenseName string
+	Classpath   string
 }
+
 func contains(s []string, str string) bool {
 	for _, v := range s {
 		if v == str {
@@ -33,13 +23,39 @@ func contains(s []string, str string) bool {
 	return false
 }
 
+func ReadCsv(filename string) ([][]string, error) {
+
+	// Open CSV file
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	// Read File into a Variable
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
+}
+
 //Still to implement the logic Licenses Array against ComplianceMatrix
-func LicenseValidation(s []string) {
-	fmt.Println("####################################")
-	fmt.Println("# From LicenseValidation function: #")
-	fmt.Println("####################################")
-	for _, license := range s {
-		fmt.Println(license)
+func LicenseValidation(s []string, CSVfilePath string) {
+
+	lines, err := ReadCsv(CSVfilePath)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, line := range lines {
+		data := CsvLine{
+			LicenseName: line[0],
+			Classpath:   line[1],
+		}
+		fmt.Println(data.LicenseName + " " + data.Classpath)
+		fmt.Println(data.Classpath)
 	}
 }
 
@@ -75,6 +91,8 @@ func LicensesExtractor(jsonFileName string) []string {
 
 func main() {
 	var jsonFileName = "javacpp_full.json"
-	var s []string = LicensesExtractor(jsonFileName)
-	LicenseValidation(s)
+	var CSVfilePath = "licenses.csv"
+	var LicenseArray []string = LicensesExtractor(jsonFileName)
+	// var records [][]string = ReadCSV(CSVfilePath)
+	LicenseValidation(LicenseArray, CSVfilePath)
 }
